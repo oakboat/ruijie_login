fddd09edb40effbaa1fae6f04b25686f18ea4816
Woo Woo <27909321+mrwoowoo@users.noreply.github.com>
2020-11-10 18:45:41 +0800

    first

--- main.go
+++ main.go
@@ -12,16 +12,17 @@ import (
 
 func main() {
 	args := os.Args
-	if args == nil || len(args) <2{
+	if args == nil || len(args) != 3 {
 		fmt.Println("使用方法:")
-		fmt.Println(os.Args[0], "你的账号", "你的密码")
+		fmt.Println("程序名", "你的账号", "你的密码")
+		fmt.Println("例如:./ruijie_login 123456 123456")
 		return
 	}
 	account := args[1]
 	password := args[2]
 	resp, err := http.Get("http://www.google.cn/generate_204")
 	if err != nil {
-        log.Fatal(err)
+        log.Fatalln(err)
     }
 	if resp.StatusCode == 204 {
 		log.Println("你已连接网络,无需登录")
@@ -45,7 +46,17 @@ func main() {
 		loginURL := strings.Replace(sbUrlSlice[0], "index.jsp", "InterFace.do?method=login", -1)
 		queryString := sbUrlSlice[1]
 		data := "userId=" + account + "&password=" + password +"&service=&queryString=" + queryString + "&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false"
-		fmt.Println(loginURL)
-		fmt.Println(data)
+		responseBody := strings.NewReader(data)
+		resp, err = http.Post(loginURL, "application/x-www-form-urlencoded; charset=UTF-8", responseBody)
+		if err != nil {
+			log.Fatalln(err)
 		}
+		defer resp.Body.Close()
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		 }
+		sb = string(body)
+		log.Println(sb)
+	}
 }
