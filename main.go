07379f083e3c97c37a00d0627433e5d8ba87dc2c
Woo Woo <27909321+mrwoowoo@users.noreply.github.com>
2020-11-10 19:49:38 +0800

    fix

--- main.go
+++ main.go
@@ -22,41 +22,41 @@ func main() {
 	password := args[2]
 	resp, err := http.Get("http://www.google.cn/generate_204")
 	if err != nil {
-        log.Fatalln(err)
-    }
+		log.Fatalln(err)
+	}
 	if resp.StatusCode == 204 {
 		log.Println("你已连接网络,无需登录")
 	} else {
-		resp, err := http.Get("http://www.google.cn/generate_204")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		sb := string(body)
-		reg1 := regexp.MustCompile(`'(.*)'`)
-		if reg1 == nil {
-			fmt.Println("regexp err")
-			return
-		}
-		sbUrl := reg1.FindAllStringSubmatch(sb, -1)[0][1]
-		sbUrlSlice := strings.Split(sbUrl, "?")
-		loginURL := strings.Replace(sbUrlSlice[0], "index.jsp", "InterFace.do?method=login", -1)
-		queryString := sbUrlSlice[1]
-		data := "userId=" + account + "&password=" + password +"&service=&queryString=" + queryString + "&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false"
-		responseBody := strings.NewReader(data)
-		resp, err = http.Post(loginURL, "application/x-www-form-urlencoded; charset=UTF-8", responseBody)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer resp.Body.Close()
-		body, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalln(err)
-		 }
-		sb = string(body)
-		log.Println(sb)
+	resp, err := http.Get("http://www.google.cn/generate_204")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	reg1 := regexp.MustCompile(`'(.*)'`)
+	if reg1 == nil {
+		fmt.Println("regexp err")
+		return
+	}
+	sbUrl := reg1.FindAllStringSubmatch(sb, -1)[0][1]
+	sbUrlSlice := strings.Split(sbUrl, "?")
+	loginURL := strings.Replace(sbUrlSlice[0], "index.jsp", "InterFace.do?method=login", -1)
+	queryString := sbUrlSlice[1]
+	data := "userId=" + account + "&password=" + password +"&service=&queryString=" + queryString + "&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false"
+	responseBody := strings.NewReader(data)
+	resp, err = http.Post(loginURL, "application/x-www-form-urlencoded; charset=UTF-8", responseBody)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb = string(body)
+	log.Println(sb)
 	}
 }
