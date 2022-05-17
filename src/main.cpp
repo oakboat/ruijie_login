719324e7d84af10773882bf7acf6cad86abf19ed
mrwoowoo <mrwoo321@outlook.com>
2022-05-17 17:53:23 +0800

    add service prarms

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <string>
 #include "httplib.h"
 
@@ -6,29 +6,35 @@ int getLoginData(httplib::Result& res, std::string& loginURL, std::string& login
 
 int main(int argc, char** argv)
 {
-	if (argc != 3)
+	if (argc < 3)
 	{
-		std::cout << "Ê¹ÓÃ·½·¨:" << std::endl
-			<< "³ÌÐòÃû ÄãµÄÕËºÅ ÄãµÄÃÜÂë" << std::endl
-			<< "ÀýÈç:./ruijie_login 123456 123456" << std::endl;
+		std::cout << "ä½¿ç”¨æ–¹æ³•:" << std::endl
+			<< "ç¨‹åºå ä½ çš„è´¦å· ä½ çš„å¯†ç  service(å¯é€‰)" << std::endl
+			<< "ä¾‹å¦‚:./ruijie_login 123456 123456" << std::endl;
 		return -1;
 	}
 	std::string username = argv[1];
 	std::string password = argv[2];
+	std::string service;
+	if (argc == 4)
+	{
+		service = argv[3];
+	}
 	httplib::Result checkResult = httplib::Client("http://www.google.cn").Get("/generate_204");
 	if (!checkResult)
 	{
-		std::cout << "µÇÂ½×´Ì¬¼ì²éÊ§°Ü" << std::endl;
+		std::cout << "ç™»é™†çŠ¶æ€æ£€æŸ¥å¤±è´¥" << std::endl;
 		return -1;
 	}
 	if (checkResult->status == 204)
 	{
-		std::cout << "ÄãÒÑÁ¬½ÓÍøÂç,ÎÞÐèµÇÂ¼" << std::endl;
+		std::cout << "ä½ å·²è¿žæŽ¥ç½‘ç»œ,æ— éœ€ç™»å½•" << std::endl;
 		return 0;
 	}
 	std::string loginURL, loginPath, queryString;
 	getLoginData(checkResult, loginURL, loginPath, queryString);
-	queryString = "userId=" + username + "&password=" + password + "&service=&queryString=" + queryString + "&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false";
+	queryString = "userId=" + username + "&password=" + password + "&service=" + service + "&queryString=" + queryString +
+		"&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false";
 	//std::cout << loginURL << std::endl;
 	//std::cout << loginPath << std::endl;
 	//std::cout << queryString << std::endl;
@@ -36,11 +42,11 @@ int main(int argc, char** argv)
 	size_t result = loginResult->body.find("\"result\":\"success\"");
 	if (result == std::string::npos)
 	{
-		std::cout << "µÇÂ¼Ê§°Ü" << std::endl;
+		std::cout << "ç™»å½•å¤±è´¥" << std::endl;
 	}
 	else 
 	{
-		std::cout << "µÇÂ¼³É¹¦" << std::endl;
+		std::cout << "ç™»å½•æˆåŠŸ" << std::endl;
 	}
 	return 0;
 }
@@ -53,7 +59,8 @@ int getLoginData(httplib::Result& res, std::string& loginURL, std::string& login
 	index3 = res->body.find("index.jsp");
 	index4 = res->body.find_first_of("?");
 	index5 = res->body.find_last_of("'");
-	if (index1 == std::string::npos || index2 == std::string::npos || index3 == std::string::npos || index4 == std::string::npos || index5 == std::string::npos)
+	if (index1 == std::string::npos || index2 == std::string::npos || index3 == std::string::npos || 
+		index4 == std::string::npos || index5 == std::string::npos)
 	{
 		return 0;
 	}
